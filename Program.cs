ca1eff548e1e8f86220a775a0071ccb81a77be5d
Yang ha min <76045212+YangHamin@users.noreply.github.com>
2022-03-25 15:51:40 +0900

    배열에 이름 저장하고 알파벳순서 정렬

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace A060_Arraysorting
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            string[] name = { "Mouse", "cow","Tiger","Rabbit" };
+            PrintArray("Before sort", name);
+
+            Array.Reverse(name);
+            PrintArray("After Reverse", name);
+
+            Array.Sort(name);
+            PrintArray("After Sort", name);
+
+            Array.Reverse(name);
+            PrintArray("After Reverse", name);
+
+        }
+        private static void PrintArray(string s, string[] name)
+        {
+            Console.WriteLine(s);
+            foreach (var n in name) 
+            Console.WriteLine("{0}",n);
+            Console.WriteLine();    
+        }
+    }
+}
